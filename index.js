2a9fe0ae90c87d35e135103fb63f4fe9ca54ccf7
gitreja <51505109+gitreja@users.noreply.github.com>
2019-07-18 18:10:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello, World! Node is working...');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Homepage! Hello world.');
+});
+
+app.listen(3000, () => console.log('listening on port 3000'));
